d726cae6080334d4bf9562f3601443fd11d232ce
humblefool96 <gupta.nil1404@gmail.com>
2020-02-09 17:17:25 +0530

    prepared for deployment

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path')
+
 const app = express();
 
 //connect database
@@ -14,16 +16,23 @@ postsRoute = require('./routes/api/posts');
 //init middleware(body parser)
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => {
-    res.send('api running');
-})
-
+//define routes
 app.use('/api/users', usersRoute);
 app.use('/api/auth', authRoute);
 app.use('/api/profile', profileRoute);
 app.use('/api/posts', postsRoute);
 
-const PORT = 5000 || process.env.PORT;
+//serve static assets in production
+if(process.env.NODE_ENV === 'production') {
+    //set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    }); 
+}
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`server started on ${PORT}`);
 })
